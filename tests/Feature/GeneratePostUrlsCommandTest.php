102839dbfe035a800708af3aa119ddf8d02fdc4e
Elliot Derhay <elliot@elliotderhay.com>
2021-12-01 13:08:06 -0500

    Add bare test cases

--- tests/Feature/GeneratePostUrlsCommandTest.php
+++ tests/Feature/GeneratePostUrlsCommandTest.php
@@ -1,7 +1,25 @@
 <?php
 
-it('has generateposturlscommand page', function () {
-    $response = $this->get('/generateposturlscommand');
+it('throws an exception if url-base argument is missing', function (): void {
 
-    $response->assertStatus(200);
+});
+
+it('throws an exception if data argument is missing', function (): void {
+
+});
+
+it('supports post data in JSON format', function (): void {
+
+});
+
+it('can output URLs as a single line of JSON', function (): void {
+
+});
+
+it('can output URLs as pretty-formatted JSON', function (): void {
+
+});
+
+it('can output URLs as multi-line plain-text output', function (): void {
+
 });
